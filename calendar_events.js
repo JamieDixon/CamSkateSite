9691acc8d5b3ec0dd561a6b34373464d3aaa8d9f
Jamie Dixon <github@jamie-dixon.co.uk>
2026-04-23 21:53:09 +0100

    Apply strategies for finding an event based on Google Calendar event

--- calendar_events.js
+++ calendar_events.js
@@ -1,3 +1,32 @@
+const directAccessStratrgy = (event) => {
+  const foundEvent = SESSION_CONFIG[event.summary.toLowerCase()];
+  return foundEvent ? { title: event.summary, ...foundEvent } : null;
+};
+
+// This strategy allows us to define alternative titles for events in the SESSION_CONFIG.
+// This is useful for events like "quads & blades" which might be entered in the calendar with
+// a slightly different title but we still want to match it to the same session configuration.
+const alternativeTitleStrategy = (event) => {
+  const [eventKey, eventValue] = Object.entries(SESSION_CONFIG).find(
+    ([key, value]) =>
+      value.altTitles &&
+      value.altTitles.some(
+        (t) => t.toLowerCase() === event.summary.toLowerCase(),
+      ),
+  );
+
+  return eventValue ? { title: eventKey, ...eventValue } : null;
+};
+
+const applyStrategies = (event) => {
+  const strategies = [directAccessStratrgy, alternativeTitleStrategy];
+  for (const strategy of strategies) {
+    const result = strategy(event);
+    if (result) return result;
+  }
+  return null;
+};
+
 const calendarEvents = (function calendarEvents() {
   const CALENDAR_ID =
     "648a32abb0a80624c5f98e8e4bfd057578a6aed5110ba2addc6f9496fa9cabb4@group.calendar.google.com";
@@ -16,6 +45,23 @@ const calendarEvents = (function calendarEvents() {
         return response.json();
       })
       .then((data) => data.items || [])
+      .then((events) => {
+        return events.reduce((agg, event) => {
+          const eventFromConfig = applyStrategies(event);
+
+          return eventFromConfig
+            ? [
+                ...agg,
+                {
+                  summary: eventFromConfig.title,
+                  start: event.start,
+                  end: event.end,
+                  ...eventFromConfig,
+                },
+              ]
+            : agg;
+        }, []);
+      })
       .catch((error) => {
         console.error("Error fetching calendar events:", error);
         throw error;
